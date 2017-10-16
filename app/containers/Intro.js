1f79ad0ef17ef48bf438f6d500cdcd56f0977275
Angela Yi-Chun Chuang <angorayc@gmail.com>
2017-10-16 15:07:09 +0100

    setup checkbox state

--- app/containers/Intro.js
+++ app/containers/Intro.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router'
 import Helmet from 'react-helmet'
+import classnames from 'classnames'
 
 class Intro extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false
+    }
+    this._onClicked = this._onClicked.bind(this)
+  }
+
+  _onClicked(e) {
+    e.preventDefault()
+    let checked = this.state
+    this.setState({
+      checked: !checked
+    })
+  }
+
   render() {
+    let { title } = this.props,
+        { checked } = this.state,
+        checkboxClasses = classnames({
+          checkbox: true,
+          checked: checked
+        })
+
     return (
       <div className="intro">
         <Helmet
-          title="Intro"
+          title="Checkbox Demo"
         />
-        <h1>Intro Page</h1>
+        <h1>Checkbox Demo</h1>
         <div>
-          <img src="/assets/images/head.png"/>
+          <a className={checkboxClasses} onClick={this._onClicked}></a>
+          <span className="label">{title}</span>
         </div>
-        <Link to="/questions">to question</Link>
       </div>
     )
   }
+}
+
+Intro.defaultProps = {
+  title: 'Checkbox'
 }
 
 function mapStateToProps() {
